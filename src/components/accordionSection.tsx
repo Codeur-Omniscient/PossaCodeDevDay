755b124b13713c112d6b966387ae01889a33577f
Codeur-Omniscient <Anthonymedin258@outlook.fr>
2024-06-22 19:20:28 +0100

    [UPDATE]:Mise à jour du compteur et Accordeon

--- src/components/accordionSection.tsx
+++ src/components/accordionSection.tsx
@@ -6,14 +6,14 @@ import {
 } from "@/components/ui/accordion";
 import { useState } from "react";
 
-import {AnimatedTooltipPreview} from "@/components/exemple"
+import { AnimatedTooltipPreview } from "@/components/exemple";
 
 // Define the types for event items and events
 interface Speaker {
-  id:number;
+  id: number;
   name: string;
   photo: string;
-  designation:string;
+  designation: string;
 }
 
 interface EventContent {
@@ -100,20 +100,17 @@ const events: Event[] = [
             },
           ],
         },
-        
       },
       // Add more items as needed
     ],
   },
 
-  /* {
+  {
     date: "21 December 2021",
     hour: "8:00am - 9:00am",
     localisation: "Summer C",
     items: [
       {
-        imgSrc:
-          "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
         title: "DIGITAL CONFERENCE INTRODUCTION",
         content: {
           title:
@@ -129,31 +126,44 @@ const events: Event[] = [
             },
 
             {
-              name: "Jane Doe",
+              id: 2,
+              name: "Jane Smith",
+              designation: "Software Engineer",
               photo:
                 "https://images.unsplash.com/photo-1506794778202-cad84cf45f1d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8MTZ8fHN0YXR1c3xlbnwwfHwwfHx8MA%3D%3D",
             },
             {
-              name: "Jean Doe",
+              id: 3,
+              name: "Jane Smith",
+              designation: "Software Engineer",
               photo:
                 "https://images.unsplash.com/photo-1551782450-a2132b4ba21d?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             },
             {
-              name: "Alice Johnson",
+              id: 4,
+              name: "Jane Smith",
+              designation: "Software Engineer",
+              photo:
+                "https://images.unsplash.com/photo-1529626455594-4ff0802cfb7e?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+            },
+            {
+              id: 5,
+              name: "Jane Smith",
+              designation: "Software Engineer",
               photo:
                 "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             },
             {
-              name: "Bob Brown",
+              id: 6,
+              name: "Jane Smith",
+              designation: "Software Engineer",
               photo:
-                "https://images.unsplash.com/photo-1535713875002-d1d0cf377fde?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
+                "https://images.unsplash.com/photo-1522071820081-009f0129c71c?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             },
             {
-              name: "Charlie Davis",
-              photo: "/profile.png",
-            },
-            {
-              name: "Fiona Harris",
+              id: 7,
+              name: "Jane Smith",
+              designation: "Software Engineer",
               photo:
                 "https://images.unsplash.com/photo-1524504388940-b1c1722653e1?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D",
             },
@@ -162,11 +172,10 @@ const events: Event[] = [
       },
       // Add more items as needed
     ],
-  }, */
+  },
 
   // Add more events as needed
 ];
-
 
 export function AccordionDemo() {
   const [focusedItem, setFocusedItem] = useState<{
@@ -204,7 +213,6 @@ export function AccordionDemo() {
               <AccordionItem key={itemIndex} value={`item-${itemIndex}`}>
                 <AccordionTrigger className="">
                   <div className="flex flex-col gap-12 items-center  md:flex-row ">
-                    
                     <div
                       className={`flex flex-col items-start space-y-2 text-left  py-8 px-10  ${focusedItem && focusedItem.eventIndex === eventIndex && focusedItem.itemIndex === itemIndex ? "bg-slate-100" : ""}`}
                       onClick={() => handleClick(eventIndex, itemIndex)}
@@ -231,14 +239,11 @@ export function AccordionDemo() {
                     </div>
                   </div>
                 </AccordionContent>
-
               </AccordionItem>
             ))}
           </Accordion>
         </div>
       ))}
-
-     
     </div>
   );
 }
